1d93c836fee513e5049569f7bf43e7a88537246c
Andersonramos2023 <andersonrsanjo@gmail.com>
2025-04-29 13:26:02 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import App from './multiversoComics.vue'
+
+createApp(App).mount('#app')
---
+++ src/multiversoComics.vue
@@ -0,0 +1,153 @@
+<template>
+
+  <!DOCTYPE html>
+  <html lang="pt-br">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/main.css">
+    <title>Muntiverso comics</title>
+  </head>
+
+  <body>
+
+    <header class="header">
+
+      <div class="logo">
+        <h1>Mutiverso comics</h1>
+      </div>
+
+      <p class="pesquisa"> Pesquisar comics?</p>
+
+      <nav class="navegacao">
+        <ul>
+          <li><a href="#">Termos</a></li>
+          <li><a href="#">Equipes </a></li>
+          <li><a href="#">Envios</a></li>
+          <li><a href="#">Devoluções</a></li>
+          <li><a href="#"><i class="fa-solid fa-cart-shopping"></i></a></li>
+
+
+         <div class="divisao">  </div>
+
+          <li><a href="#">Cadastro</a></li>
+
+          <div class="divisao">  </div>
+
+          <li><a href="#">carinho</a></li>
+        </ul>
+
+      </nav>
+    </header>
+
+    <div class="linha"></div>
+
+    <main>
+
+      <section class="apresentacao">
+
+
+        <h2>Bem-vindo ao Mutiverso Comics</h2>
+        <div>
+          <a href="#">Autor da marvel</a>
+        </div>
+
+        <h1>Stanley Martin Lieber</h1>
+
+        <p>O principal autor e editor da Marvel Comics foi Stan Lee (Stanley Martin Lieber), um escritor, editor,
+          produtor e ator norte-americano. Ele é conhecido por ter co-criado muitos dos personagens icônicos da Marvel
+          Comics, como o Homem-Aranha, os X-Men, o Hulk, Thor, o Homem de Ferro e o Quarteto Fantástico. </p>
+
+        <div class="botao"><a href="#">Acessar página do autor</a></div>
+
+        <!-- //////// FALTA COLOCAR A IMG ///////-->
+      </section>
+      <div class="linha3"></div>
+
+      <section class="indicacao">
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <a href="#">Frete grátis para SC</a>
+              </li>
+
+              <div class="reverce"></div>
+
+              <li>
+                <a href="#">Livros recomendados</a>
+              </li>
+
+              <div class="reverce"></div>
+                
+              <li>
+                <a href="#">Mais vendidos</a>
+
+                <div class="sublinhado"></div>
+
+              </li>
+            </ul>
+          </nav>
+        </div>
+
+      </section>
+
+      <div class="linha3"></div>
+
+        <section class="comics">
+
+          <h1>
+            Lansamentos novos
+          </h1>
+          <div class="quadrinhos">
+
+            <nav>
+
+              <ul class="ajuste" >
+
+                <li>
+                  <img src="/public/images/images.jpeg" alt="#">
+
+                  <h3>Invencible</h3>
+
+                  <p>Robert Kirkman</p>
+
+                  <a href="#">Compra</a>
+                </li>
+
+
+
+                <li>
+                  <img src="/public/images/images (1).jpeg" alt="#">
+                  <h3>Guerra civil</h3>
+                  <p>Stanley Martin Lieber</p>
+
+                  <a href="#">Compra</a>
+
+                </li>
+                <li>
+                    <img src="/public/images/images (2).jpeg" alt="#">
+
+                    <h3>Homem aranha</h3>
+
+                    <p>Stanley Martin Lieber</p>
+                    <p> </p>
+                    <a href="#">Compra</a>
+                </li>
+              </ul>
+            </nav>
+
+          </div>
+
+        </section>
+        <footer>
+          <section class="footer">
+
+
+          </section>
+        </footer>
+    </main>
+  </body>
+
+  </html>
+</template>
